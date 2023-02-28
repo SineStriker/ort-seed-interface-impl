e32d33c87b64ec77672b2ce673bf63ea8f87ea27
yqzhishen <yangqian_1015@icloud.com>
2023-02-28 16:32:11 +0800

    Add example for random inference

--- csharp/TestSetSeed/Program.cs
+++ csharp/TestSetSeed/Program.cs
@@ -1,5 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Reflection;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
 namespace TestSetSeed;
 
 public static class Program
@@ -9,14 +13,41 @@ public static class Program
         var seed = OrtSeedImpl.Library.GetRandomSeed();
         Console.WriteLine($"Original seed: {seed}");
 
-        OrtSeedImpl.Library.SetRandomSeed(seed = new Random().NextInt64(0, Int64.MaxValue));
+        seed = new Random().NextInt64(0, Int64.MaxValue);
+        
+        OrtSeedImpl.Library.SetRandomSeed(seed);
         Console.WriteLine($"Set a seed: {seed}");
 
         seed = OrtSeedImpl.Library.GetRandomSeed();
         Console.WriteLine($"Current seed: {seed}");
 
+        var str1 = InferRandomModule().GetArrayString();
+        
+        OrtSeedImpl.Library.SetRandomSeed(seed);
+        Console.WriteLine($"Set a seed: {seed}");
+
+        seed = OrtSeedImpl.Library.GetRandomSeed();
+        Console.WriteLine($"Current seed: {seed}");
+
+        var str2 = InferRandomModule().GetArrayString();
+
+        Console.WriteLine(str1 == str2 ? "Output tensors match!" : "Output tensors mismatch!");
+
         Console.WriteLine($"Press enter to exit...");
         Console.ReadLine();
         return 0;
+    }
+
+    public static Tensor<float> InferRandomModule()
+    {
+        var modelPath = Path.Join(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "models", "random.onnx");
+        var session = new InferenceSession(modelPath);
+        var input = new DenseTensor<float>(new[] { 1, 32, 5 });
+        input.Fill(0f);
+        var x = NamedOnnxValue.CreateFromTensor("x", input);
+        var y = session.Run(new[] { x }).ElementAt(0);
+        var output = y.AsTensor<float>();
+        return output;
     }
 }
